f4fc4dc966dd6f6fc77ca415ed2ec9203a088790
AyrtonE01 <ayrton.enokida123@gmail.com>
2026-03-23 21:28:11 -0300

    Commit Entity

---
+++ src/main/java/com/produto/produto/configuration/Swagger.java
@@ -0,0 +1,7 @@
+package com.produto.produto.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+}
---
+++ src/main/java/com/produto/produto/entity/ProdutoEntity.java
@@ -0,0 +1,63 @@
+package com.produto.produto.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity(name = "Produto")
+@Table(name = "tb_produto")
+public class ProdutoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_produto")
+    private String nome;
+
+    @Column(name = "valor_preco")
+    private BigDecimal preco;
+
+    private Integer estoque;
+
+    public ProdutoEntity() {
+    }
+
+    public ProdutoEntity(Long id, String nome, BigDecimal preco, Integer estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
