4acdcd6a59f4dabea04df2a04fb882d3b80e66ac
AyrtonE01 <ayrton.enokida123@gmail.com>
2026-03-23 21:29:24 -0300

    Commit Service

---
+++ src/main/java/com/produto/produto/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package com.produto.produto.service;
+
+import com.produto.produto.controller.ProdutoController;
+import com.produto.produto.entity.ProdutoEntity;
+import com.produto.produto.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public ProdutoEntity salvar(ProdutoEntity produto) {
+        return repository.save(produto);
+    }
+
+    public List<ProdutoEntity> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<ProdutoEntity> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
