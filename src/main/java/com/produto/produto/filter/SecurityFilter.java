85816a33894467810abf593352375a2f2632fed9
AyrtonE01 <ayrton.enokida123@gmail.com>
2026-05-25 22:22:14 -0300

    Implementação do SecurityFilter

---
+++ src/main/java/com/produto/produto/filter/SecurityFilter.java
@@ -0,0 +1,27 @@
+package com.produto.produto.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.GET,"/produtos").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/produtos").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/produtos**").permitAll())
+                .build();
+    }
+
+}
